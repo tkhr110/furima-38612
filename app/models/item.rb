ec244f37a801ce377af8e1d55208735d7f7d1008
tkhr110 <goldenkiwi1003@gmail.com>
2022-11-10 15:56:37 +0900

    rubocop-a

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,5 +16,6 @@ class Item < ApplicationRecord
   validates :dfee_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :dday_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 end
