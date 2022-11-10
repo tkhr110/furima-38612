3a9c1ad4591778b1e9bdb55e1cd9b47000392b63
tkhr110 <goldenkiwi1003@gmail.com>
2022-11-10 15:39:48 +0900

    price validatesの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,5 @@ class Item < ApplicationRecord
   validates :dfee_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :dday_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'Half-width number' },
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :price, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 end
