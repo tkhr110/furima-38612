bbb609f1cefa381804f046b049c6ba9038028207
tkhr110 <goldenkiwi1003@gmail.com>
2022-11-14 16:29:54 +0900

    rubocop -a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
-    render :edit
+      render :edit
     end
   end
 
