b136dd82480debc472902e530f05a71e158038c4
tkhr110 <goldenkiwi1003@gmail.com>
2022-11-14 10:47:01 +0900

    詳細ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show,]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -17,10 +18,17 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :dfee_id, :area_id, :dday_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
